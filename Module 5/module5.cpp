5bb3a43656a76edcbd5bb70917a68f89c657e470
grajdeep51 <131944745+grajdeep51@users.noreply.github.com>
2026-03-22 22:56:35 -0700

    Update module5.cpp

--- Module 5/module5.cpp
+++ Module 5/module5.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
+
 using namespace std;
 
-void showMenu() {
-cout << "1. Add\n2.View\n3. Exit\n";
+// Function for option 1
+void addNumbers() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum: " << a + b << endl;
 }
-int main() {
-int option;
-do{
-showMenu();
-cin >> option;
-} while (option != 3);
-return 0;
 
+// Function for option 2
+void viewMessage() {
+    cout << "This is a simple menu program." << endl;
+}
+
+// Function to display menu
+void showMenu() {
+    cout << "\n1. Add\n2. View\n3. Exit\n";
+}
+
+int main() {
+
+    int option;
+
+    do {
+        showMenu();
+        cin >> option;
+
+        // Call functions based on user choice
+        if (option == 1) {
+            addNumbers();
+        }
+        else if (option == 2) {
+            viewMessage();
+        }
+        else if (option == 3) {
+            cout << "Exiting program..." << endl;
+        }
+        else {
+            cout << "Invalid choice." << endl;
+        }
+
+    } while (option != 3);
+
+    return 0;
 }
